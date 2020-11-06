acb1a1f6a011dba65b26d4997aec1dce62864b2b
dbik <dbikas88@gmail.com>
2020-11-06 17:26:46 +0200

    Last Updates.

--- src/main.js
+++ src/main.js
@@ -25,23 +25,20 @@ Vue.config.productionTip = false;
 Vue.prototype.$caseManagerAndAbove = [
     "organization_manager",
     "coordinator",
-    "case_manager"
+    "case_manager",
 ];
 Vue.prototype.$networkManagerAndAbove = [
     "organization_manager",
     "coordinator",
-    "network_manager"
+    "network_manager",
 ];
 
 Vue.use(VueGoogleMaps, {
     load: {
-        // key: "AIzaSyDetsjT3ifagYvxnjgF2zYtYAS-2Ni_zxQ", // suite
-        // key: "AIzaSyAg9utw_PA8gyI8fLXS5E6SqrO31lw4cxE", // mine-try
-        // key: "AIzaSyCWo-tJW4X1gVEsDtz67j7coxDIxje0gRk", // production
-        key: "AIzaSyCQoVzpBSZPGyit_1D1gz4M_Rdo6KUiWCs",
-        libraries: "places" // necessary for places input
+        key: "xxxxx",
+        libraries: "places", // necessary for places input
         // callback: initService(),
-    }
+    },
 });
 
 Vue.component(VImageInput.name, VImageInput);
@@ -50,5 +47,5 @@ new Vue({
     i18n,
     router,
     store,
-    render: h => h(App)
+    render: (h) => h(App),
 }).$mount("#app");
